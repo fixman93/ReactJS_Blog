38f3e1367962829f0ddaff38053cf948fa43157b
Boris Civcic <civcicboris93@gmail.com>
2018-08-11 16:43:19 +0200

    show all posts added

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,17 +1,29 @@
+import _ from 'lodash'
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { fetchPosts } from '../actions'
 
 class PostsIndex extends Component {
   componentDidMount () {
-    this.props.fetchPost
+    this.props.fetchPosts()
   }
 
+  renderPosts() {
+    return _.map(this.props.posts, post => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          {post.title}
+        </li>
+      )
+    })
+  }
   render () {
-    console.log(this.props.posts)
     return (
       <div>
-        Posts Index
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
       </div>
     )
   }
@@ -21,4 +33,4 @@ function mapStateToProps(state) {
   return { posts: state.posts }
 }
 
-export default connect(mapStateToProps, { fetchPosts })(PostsIndex)
+  export default connect(mapStateToProps, { fetchPosts })(PostsIndex)
